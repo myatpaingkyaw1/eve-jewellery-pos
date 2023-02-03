533a53cf193576852f6bd98b64b15a2ffdfc4a10
myatpaingkyaw1 <myatpaingkyaw@example.com>
2023-02-03 21:51:14 +0630

    add design in header

--- src/Header.js
+++ src/Header.js
@@ -71,9 +71,32 @@ function Header() {
                             </div>
                             <a href="#" class="remove"><i class="tf-ion-close"></i></a>
                         </div>
-                        
+
+                        <div class="media">
+                            <a href="/product-single">
+                                <img class="media-object img-mr-3" src="assets/images/cart-2.jpg" alt="img"></img>
+                            </a>
+                            <div class="media-body">
+                                <h6>Ladies Bag</h6>
+                                <div class="cart-price">
+                                    <span>1 x</span>
+                                    <span>1250.00KS</span>
+                                </div>
+                            </div>
+                            <a href="#" class="remove"><i class="tf-ion-close"></i></a>
+                        </div>
+
+                        <div class="cart-summary">
+                            <span class="h6">Total</span>
+                            <span class="total-price h6">$1799.00</span>
+                            <div class="text-center cart-buttons mt-3">
+                                <a href="#" class="btn btn-small btn-transparent btn-block">View Cart</a>
+                                <a href="#" class="btn btn-small btn-main btn-block">Checkout</a>
+                            </div>
+                        </div>
                     </div>
                 </li>
+                <li class="list-inline-item"><a href="#"><i class="tf-ion-ios-person mr-3"></i></a></li>
             </ul>
         </div>
     </nav>
